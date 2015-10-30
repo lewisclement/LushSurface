1f3129672871814e421c4c89137958cd34af6b93
lewisclement <lewisclement@gmail.com>
2015-10-30 23:00:35 +0100

    Initial setup

---
+++ LushSurface/pch.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#ifndef PCH_H
+#define PCH_H
+
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <cmath>
+#include <string>
+#include <algorithm>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <GL/glew.h>
+#include <GL/glu.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+#pragma clang diagnostic ignored "-Wundefined-reinterpret-cast"
+#pragma clang diagnostic ignored "-Wshadow"
+#pragma clang diagnostic ignored "-Wundef"
+#pragma clang diagnostic ignored "-Wfloat-equal"
+
+#define GLM_FORCE_RADIANS
+
+#include "../glm/glm/glm.hpp"
+#include "../glm/glm/gtc/type_ptr.hpp"
+#include "../glm/glm/gtc/matrix_transform.hpp"
+
+#pragma clang diagnostic pop
+
+#endif
